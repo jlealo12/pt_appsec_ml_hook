1ceee41c2e588dd6f1e664b4e8db8b2b2b99a704
jlealo12 <92340459+jlealo12@users.noreply.github.com>
2025-09-24 22:24:06 -0500

    feat: initial commit

---
+++ hooks/hook.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main():
+    print("This is the main function of the hook module.")
+    print("Arguments passed to the script:", sys.argv)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
